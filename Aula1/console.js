d6b2ece486eda534deac005d99a601a6c29d11cb
unknown <pedro.rpg@protonmail.com>
2021-11-18 21:25:44 -0300

    adição de console.js

---
+++ Aula1/console.js
@@ -0,0 +1,16 @@
+// log -> registro
+console.log();
+
+// error -> exibe mensagens de erro
+console.error("Deu erro!");
+
+// table -> exibe informações tabuladas de forma mais organizada
+let lista = ["ayo", "what da dog doin", 55];
+console.table(lista);
+
+// time & timeEnd -> exibe o tempo necessário para realização de uma operação.
+console.time();
+console.timeEnd();
+
+// trace -> exibe a stacktrace de todos os pontos do código
+console.trace();
